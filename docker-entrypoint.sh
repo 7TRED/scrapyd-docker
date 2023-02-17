017abff38a5a09ca1ec2953b4970de0ad97b3254
7TRED <ms1514094@gmail.com>
2023-02-17 06:44:47 +0530

    removed storing items in items_dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+if [ ! -z "$ENABLE_ITEMS_DIR" ] && [ "$ENABLE_ITEMS_DIR" = "True" ]; then
+    sed -i 's/items_dir = /items_dir = \/var\/lib\/scrapyd\/items/g' /etc/scrapyd/scrapyd.conf
+fi
+
 systemctl start scrapyd.service
 
 exec "$@"
